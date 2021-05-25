23084eb64364a7e5848f70e09cf197142a05d41d
Natthan Guilhot <natthan.guilhot@laposte.net>
2021-05-25 17:02:15 +0200

    carrousel v1.5

---
+++ js/index.js
@@ -0,0 +1,19 @@
+import {nextCarrousel} from './script.js';
+import {previousCarrousel} from './script.js';
+
+// CARROUSEL
+let arrayImgCarrousel = document.querySelectorAll('.img_carrousel'); // On selectionne toutes les images du carousel
+let previousImgCarrousel = document.querySelector('.previous_img_carrousel'); // On selectionne les fleches
+let nextImgCarrousel = document.querySelector('.next_img_carrousel');
+nextImgCarrousel.addEventListener('click', function(){ // Au click de la fleche,
+  nextCarrousel(arrayImgCarrousel);
+});
+previousImgCarrousel.addEventListener('click', function(){ // Même traitement mais avec la fleche précédente
+  previousCarrousel(arrayImgCarrousel);
+});
+//
+
+setInterval(() => {
+    nextCarrousel(arrayImgCarrousel);
+  }, 10000);
+  
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Animation d'apparition
 (function() {
   var elements;
   var windowHeight;
@@ -25,68 +26,48 @@
   init();
   checkPosition();
 })();
-
-// CAROUSEL
-let arrayImgCarousel = document.querySelectorAll('.img_carousel'); // On selectionne toutes les images du carousel
-let previousImgCarousel = document.querySelector('.previous_img_carousel'); // On selectionne les fleches
-let nextImgCarousel = document.querySelector('.next_img_carousel');
-nextImgCarousel.addEventListener('click', function(){ // Au click de la fleche,
-  for (const [i, img] of arrayImgCarousel.entries()) { // Pour chaque img,
-    if(arrayImgCarousel[arrayImgCarousel.length-1].classList.contains('flex')) { // Si l'image est la dernière, alors on reaffiche la première
-      arrayImgCarousel[arrayImgCarousel.length-1].classList.add('NextTranslateXActual');
-      setTimeout(() => {
-        arrayImgCarousel[arrayImgCarousel.length-1].classList.replace('flex', 'hidden');
-        arrayImgCarousel[arrayImgCarousel.length-1].classList.remove('NextTranslateXActual');
-      }, 500);
-      arrayImgCarousel[0].classList.replace('hidden',"flex");
-      arrayImgCarousel[0].classList.add('NextTranslateXNext');
-      setTimeout(() => {
-        arrayImgCarousel[0].classList.remove('NextTranslateXNext');
-      }, 500);
+//
+// CARROUSEL
+export function nextCarrousel (arrayImgCarrousel) {
+  for (const [i, img] of arrayImgCarrousel.entries()) { // Pour chaque img,
+    if(arrayImgCarrousel[arrayImgCarrousel.length-1].classList.contains('flex')) { // Si l'image est la dernière, alors on reaffiche la première
+      arrayImgCarrousel[0].classList.remove('NextTranslateXNext');
+      arrayImgCarrousel[arrayImgCarrousel.length-1].classList.remove('NextTranslateXActual');
+      arrayImgCarrousel[arrayImgCarrousel.length-1].classList.replace('flex', 'hidden');
+      arrayImgCarrousel[arrayImgCarrousel.length-1].classList.add('NextTranslateXActual');
+      arrayImgCarrousel[0].classList.add('NextTranslateXNext');
+      arrayImgCarrousel[0].classList.replace('hidden',"flex");
       break
     }else if(img.classList.contains('flex')) { // Sinon on désaffiche l'image acutelle et on affiche l'image suivante (i+1)
+      arrayImgCarrousel[i+1].classList.remove('NextTranslateXNext');
+      img.classList.remove('NextTranslateXActual');
+      img.classList.replace('flex','hidden');
       img.classList.add('NextTranslateXActual');
-      setTimeout(() => {
-        img.classList.replace('flex','hidden');
-        img.classList.remove('NextTranslateXActual');
-      }, 500);
-      arrayImgCarousel[i+1].classList.replace('hidden','flex');
-      arrayImgCarousel[i+1].classList.add('NextTranslateXNext');
-      setTimeout(() => {
-        arrayImgCarousel[i+1].classList.remove('NextTranslateXNext');
-      }, 500);
+      arrayImgCarrousel[i+1].classList.replace('hidden','flex');
+      arrayImgCarrousel[i+1].classList.add('NextTranslateXNext');
       break
     }
   }
-});
-previousImgCarousel.addEventListener('click', function(){ // Même traitement mais avec la fleche précédente
-  for (const [i, img] of arrayImgCarousel.entries()) {
-    if(arrayImgCarousel[0].classList.contains('flex')) {// Si l'image est la première, alors on reaffiche la dernière
-      arrayImgCarousel[0].classList.add('PreviousTranslateXActual');
-      setTimeout(() => {
-        arrayImgCarousel[0].classList.replace('flex', 'hidden');
-        arrayImgCarousel[0].classList.remove('PreviousTranslateXActual');
-      }, 500);
-      arrayImgCarousel[arrayImgCarousel.length-1].classList.replace('hidden',"flex");
-      arrayImgCarousel[arrayImgCarousel.length-1].classList.add('PreviousTranslateXNext');
-      setTimeout(() => {
-        arrayImgCarousel[arrayImgCarousel.length-1].classList.remove('PreviousTranslateXNext');
-      }, 500);
-
+};
+export function previousCarrousel (arrayImgCarrousel) {
+  for (const [i, img] of arrayImgCarrousel.entries()) {
+    if(arrayImgCarrousel[0].classList.contains('flex')) {// Si l'image est la première, alors on reaffiche la dernière
+      arrayImgCarrousel[arrayImgCarrousel.length-1].classList.remove('PreviousTranslateXNext');
+      arrayImgCarrousel[0].classList.remove('PreviousTranslateXActual');
+      arrayImgCarrousel[0].classList.replace('flex', 'hidden');
+      arrayImgCarrousel[0].classList.add('PreviousTranslateXActual');
+      arrayImgCarrousel[arrayImgCarrousel.length-1].classList.replace('hidden',"flex");
+      arrayImgCarrousel[arrayImgCarrousel.length-1].classList.add('PreviousTranslateXNext');
       break
     }else if(img.classList.contains('flex')) { // Sinon on désaffiche l'image acutelle et on affiche l'image précédente (i+1)
+      arrayImgCarrousel[i-1].classList.remove('PreviousTranslateXNext');
+      img.classList.remove('PreviousTranslateXActual');
+      img.classList.replace('flex','hidden');
       img.classList.add('PreviousTranslateXActual');
-      setTimeout(() => {
-        img.classList.replace('flex','hidden');
-        img.classList.remove('PreviousTranslateXActual');
-      }, 500);
-      arrayImgCarousel[i-1].classList.replace('hidden','flex');
-      arrayImgCarousel[i-1].classList.add('PreviousTranslateXNext');
-      setTimeout(() => {
-        arrayImgCarousel[i-1].classList.remove('PreviousTranslateXNext');
-      }, 500);
+      arrayImgCarrousel[i-1].classList.replace('hidden','flex');
+      arrayImgCarrousel[i-1].classList.add('PreviousTranslateXNext');
       break
     }
   }
-});
+};
 //
